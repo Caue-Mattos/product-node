e62ddb470b5dacb5c2dd7b81ea7ab1345534e64b
Caue-Mattos <cauefelipe21@gmail.com>
2024-09-20 09:00:28 -0300

    att

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,4 @@
-import { getAllProducts } from "../services/productService.js"
+import { createProduct,getAllProducts, getProductById } from "../services/productService.js"
 export const getProducts = async(req, res) => {
     try {
         const products = await getAllProducts();
@@ -8,5 +8,30 @@ export const getProducts = async(req, res) => {
         res.status(500).json({
             error: "Erro ao buscar produtos",
         });
+    };
+};
+
+export const createProductController = async(req,res) => {
+    try {
+        const newProduct = await createProduct(req.body);
+        res.status(201).json(newProduct)
+    }
+    catch (error) {
+        res.status(500).json({error: "Erro ao criar produto" })
+    };
+};
+
+export const getProduct = async(req, res)=>{
+    try {
+        const product = await getProductById(req.params.id)
+        if (product) {
+            res.json(product);
+        }
+        else {
+            res.status(500).json({error: "produto não encontrado"})
+        }
+    }
+    catch (erro){
+        res.status(500).json({error: "erro ao buscar o produto"})
     }
 };
